988fc43ee04e79dfd0979613c84c4f8e855748f8
Photomotch <info@aflame.co.jp>
2015-06-05 14:54:12 +0900

    Compare 2 patterns!

--- test.bash
+++ test.bash
@@ -1,8 +1,18 @@
 #!/bin/bash
 
-ans=$(./15745128 35 28)
+ans1=$(./15745128 35 28)
 
-if [ "$ans" -eq 7 ]
+if [ "$ans1" -eq 7 ]
+then
+	echo "OK!"
+else
+	echo "NG!"
+	exit 1
+fi
+
+ans2=$(./15745128 117 104)
+
+if [ "$ans2" -eq 13 ]
 then
 	echo "OK!"
 	exit 0
